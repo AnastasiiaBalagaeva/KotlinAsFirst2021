c668ecde428be3913158c0b117106e5edde4e77b
AnastasiiaBalagaeva <abalagaevaa@mail.ru>
2021-09-11 16:15:50 +0300

    first task solved

--- src/lesson1/task1/Simple.kt
+++ src/lesson1/task1/Simple.kt
@@ -74,7 +74,8 @@ fun seconds(hours: Int, minutes: Int, seconds: Int): Int = hours * 3600 + minute
  * Определить длину того же отрезка в метрах (в данном случае 18.98).
  * 1 сажень = 3 аршина = 48 вершков, 1 вершок = 4.445 см.
  */
-fun lengthInMeters(sagenes: Int, arshins: Int, vershoks: Int): Double = sagenes * 48 * 0.04445 + arshins * 16 * 0.04445 + vershoks * 0.04445
+fun lengthInMeters(sagenes: Int, arshins: Int, vershoks: Int): Double = 0.04445 * (sagenes * 48 +
+        arshins * 16 + vershoks)
 
 /**
  * Тривиальная (1 балл)
@@ -82,7 +83,8 @@ fun lengthInMeters(sagenes: Int, arshins: Int, vershoks: Int): Double = sagenes 
  * Пользователь задает угол в градусах, минутах и секундах (например, 36 градусов 14 минут 35 секунд).
  * Вывести значение того же угла в радианах (например, 0.63256).
  */
-fun angleInRadian(deg: Int, min: Int, sec: Int): Double = deg * PI/180 + min * PI/(180*60) + sec * PI/(180*60*60)
+fun angleInRadian(deg: Int, min: Int, sec: Int): Double = deg * PI / 180 + min * PI / (180 * 60) +
+        sec * PI / (180 * 60 * 60)
 
 /**
  * Тривиальная (1 балл)
@@ -111,7 +113,8 @@ fun thirdDigit(number: Int): Int = number / 100 % 10
  * прибыл на станцию назначения в h2 часов m2 минут того же дня (например в 13:01).
  * Определите время поезда в пути в минутах (в данном случае 216).
  */
-fun travelMinutes(hoursDepart: Int, minutesDepart: Int, hoursArrive: Int, minutesArrive: Int): Int = abs((hoursDepart - hoursArrive) * 60 + minutesDepart - minutesArrive)
+fun travelMinutes(hoursDepart: Int, minutesDepart: Int, hoursArrive: Int, minutesArrive: Int): Int =
+    abs((hoursDepart - hoursArrive) * 60 + minutesDepart - minutesArrive)
 
 /**
  * Простая (2 балла)
@@ -124,7 +127,7 @@ fun travelMinutes(hoursDepart: Int, minutesDepart: Int, hoursArrive: Int, minute
 
 fun accountInThreeYears(initial: Int, percent: Int): Double {
     val z = (1 + 0.01 * percent)
-    return z.pow(2) * initial
+    return z * z * z * initial
 }
 
 /**
@@ -133,4 +136,4 @@ fun accountInThreeYears(initial: Int, percent: Int): Double {
  * Пользователь задает целое трехзначное число (например, 478).
  * Необходимо вывести число, полученное из заданного перестановкой цифр в обратном порядке (например, 874).
  */
-fun numberRevert(number: Int): Int = (number % 10) * 100 + ((number/10) % 10) * 10 + number/100
+fun numberRevert(number: Int): Int = (number % 10) * 100 + ((number / 10) % 10) * 10 + number / 100
