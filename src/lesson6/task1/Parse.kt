f47d46b78dc7df767dd60708950dc0d3f7edcf3e
AnastasiiaBalagaeva <abalagaevaa@mail.ru>
2021-11-14 15:25:20 +0300

    6ой модуль

--- src/lesson6/task1/Parse.kt
+++ src/lesson6/task1/Parse.kt
@@ -148,7 +148,22 @@ fun flattenPhoneNumber(phone: String): String = TODO()
  * Прочитать строку и вернуть максимальное присутствующее в ней число (717 в примере).
  * При нарушении формата входной строки или при отсутствии в ней чисел, вернуть -1.
  */
-fun bestLongJump(jumps: String): Int = TODO()
+fun bestLongJump(jumps: String): Int {
+    if (jumps.contains(Regex("""[^\d\s\-%]""")))
+        return -1
+    else {
+        val list = jumps.split(" ")
+        for (i in list) {
+            try {
+                val number = i.toInt()
+                return list.maxOrNull()!!.toInt()
+            } catch (e: NumberFormatException) {
+                return -1
+            }
+        }
+    }
+    return -1
+}
 
 /**
  * Сложная (6 баллов)
